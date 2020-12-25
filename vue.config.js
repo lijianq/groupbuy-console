8fb3aab9956f02f1db78e49678d9f2cc0631d2ae
wdajun1981 <51494794@qq.com>
2020-12-25 20:18:07 +0800

    修改webpack支持vue-svg-loader

--- vue.config.js
+++ vue.config.js
@@ -25,6 +25,22 @@ const vueConfig = {
     },
     chainWebpack: (config) => {
         config.resolve.alias.set('@$', resolve('src'))
+
+        const svgRule = config.module.rule('svg');
+        svgRule.uses.clear();
+        svgRule
+            .oneOf('inline')
+            .resourceQuery(/inline/)
+            .use('vue-svg-loader')
+            .loader('vue-svg-loader')
+            .end()
+            .end()
+            .oneOf('external')
+            .use('file-loader')
+            .loader('file-loader')
+            .options({
+                name: 'assets/[name].[hash:8].[ext]'
+            })
     },
     css: {
         loaderOptions: {
