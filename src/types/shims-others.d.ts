33180e70be2f53d7583c439fdf45548ba570f1a3
wdajun1981 <51494794@qq.com>
2020-12-27 09:16:07 +0800

    解决svg图标显示问题

--- src/types/shims-others.d.ts
+++ src/types/shims-others.d.ts
@@ -3,3 +3,7 @@ declare module 'webpack-theme-color-replacer/client'
 declare module 'cos-js-sdk-v5'
 declare module 'lodash.get'
 declare module 'lodash.pick'
+declare module '*.svg?inline' {
+    const content: any
+    export default content
+}
